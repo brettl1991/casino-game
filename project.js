3392e476ee77a0488b78f93ebd5e8a358a0cbc89
brettl1991 <brettl1991@gmail.com>
2023-06-19 13:35:28 +0100

    feat: determine  the user deposit

--- project.js
+++ project.js
@@ -5,3 +5,21 @@
 // 5. Check if the user won
 // 6. Give the user their winning
 // 7. Play again
+
+// 1.
+
+const prompt = require("prompt-sync")();
+const deposit = () => {
+  while (true) {
+    const depositAmount = prompt("Enter a deposit amount: ");
+    const numberDepositAmont = parseFloat(depositAmount);
+    if (isNaN(numberDepositAmont) || numberDepositAmont <= 0) {
+      console.log("Invalid deposit amount, try again!");
+    } else {
+      return numberDepositAmont;
+    }
+  }
+};
+
+const depositAmount = deposit();
+console.log(depositAmount);
