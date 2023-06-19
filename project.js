faa5f916a7bf684b4ba0f40c2664386c21a7ad6f
brettl1991 <brettl1991@gmail.com>
2023-06-19 13:16:18 +0100

    feat: initialise project and create project plan

---
+++ project.js
@@ -0,0 +1,7 @@
+// 1. Deposit some money
+// 2. Determine number of lines to bet on
+// 3. Collect bet amount
+// 4. Spin the slot machine
+// 5. Check if the user won
+// 6. Give the user their winning
+// 7. Play again
